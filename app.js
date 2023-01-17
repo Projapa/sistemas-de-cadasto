96b6ab3fc59255c717809cc0108b5a56c6d87bb6
Projapa <121911759+Projapa@users.noreply.github.com>
2023-01-17 13:21:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 console.log(¨Rodando o sistema de gerenciamento e jogos¨)
-console.log (aplicação está se conectando ao banco e dados¨)
+console.log (¨aplicação está se conectando ao banco e dados¨)
