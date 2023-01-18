a4cb6f6544573bfba77d42b854cf77a62832210f
Projapa <121911759+Projapa@users.noreply.github.com>
2023-01-18 12:14:06 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 console.log("Rodando o sistema de gerenciamento e jogos")
 console.log("aplicação está se conectando ao banco e dados")
+//mais um console log de teste:
+console.log("O Paulo fez esse commit sem voce ver na aula!")
+console.log("Enviando para o Github")
+
+
